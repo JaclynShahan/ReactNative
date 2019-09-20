eeff921eda6b13907f82199beeed71aecfeb5b66
Jaclyn <jaclyndshahan@gmail.com>
2019-09-19 19:59:01 -0500

    used image component

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'React';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 class App extends Component {
 render() {
+  let picture = {
+    uri:
+    'http://www.soulandspiritmagazine.com/wp-content/uploads/2015/04/omg-cute-things-112912_08-590x472.jpg'
+  };
   return (
     <View style={{flex: 1, justifyContent: "center" , alignItems: "center"}}>
       <Text>
         Hello, World!
       </Text>
+    
+    <Image source={picture} style={{width: 200, height: 175}}/>
     </View>
   )
 }
